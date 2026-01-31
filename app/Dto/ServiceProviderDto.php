7b0ab5c12346acd1428f40c27faf09fda4d76c4b
TjayAmit <tristanjayamit0813@gmail.com>
2026-01-31 16:06:14 +0800

    feat: add guard clauses for required fields and email validation

--- app/Dto/ServiceProviderDto.php
+++ app/Dto/ServiceProviderDto.php
@@ -5,7 +5,7 @@ namespace App\Dto;
 use App\Enums\ServiceProviderStatusEnum;
 use App\Enums\ServiceTypeEnum;
 
-class ServiceProviderDto
+readonly class ServiceProviderDto
 {
     public function __construct(
         public string $name,
@@ -19,14 +19,26 @@ class ServiceProviderDto
 
     public static function fromArray(array $data): ServiceProviderDto
     {
+        foreach(['name', 'email', 'service_type', 'status'] as $field){
+            if (!isset($data[$field])) {
+                throw new \InvalidArgumentException("The field $field is required");
+            }
+        }
+
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException("Invalid email");
+        }
+
         return new self(
-            name: $data['name'],
-            email: $data['email'],
-            serviceType: $data['service_type'],
-            status: $data['status'],
-            id: $data['id'],
-            url: $data['url'],
-            apiKey: $data['api_key']
+            name: trim($data['name']),
+            email: strtolower(trim($data['email'])),
+            serviceType: $data['service_type'] instanceof ServiceTypeEnum ?
+                $data['service_type']: ServiceTypeEnum::from($data['service_type']),
+            status: $data['status'] instanceof ServiceProviderStatusEnum ?
+                $data['status']: ServiceProviderStatusEnum::from($data['status']),
+            id: $data['id'] ?? null,
+            url: $data['url'] ?? null,
+            apiKey: $data['api_key'] ?? null,
         );
     }
 
@@ -35,11 +47,11 @@ class ServiceProviderDto
         return [
             'name' => $this->name,
             'email' => $this->email,
-            'service_type' => $this->serviceType,
-            'status' => $this->status,
+            'service_type' => $this->serviceType->value,
+            'status' => $this->status->value,
             'id' => $this->id,
             'url' => $this->url,
-            'apiKey' => $this->apiKey,
+            'api_key' => $this->apiKey,
         ];
     }
 }
