93868f2c59d61017a22c744305767e51172ff48b
TjayAmit <tristanjayamit0813@gmail.com>
2026-01-31 15:18:47 +0800

    feat: introduct ServiceProviderDto for converting data for storing from array form - added pest package - added ServiceProviderDtoTest and test conduct

--- app/Dto/ServiceProviderDto.php
+++ app/Dto/ServiceProviderDto.php
@@ -1,28 +1,30 @@
 <?php
 
-namespace App\DTO;
+namespace App\Dto;
 
 use App\Enums\ServiceProviderStatusEnum;
 use App\Enums\ServiceTypeEnum;
 
-class ServiceProviderDTO
+class ServiceProviderDto
 {
     public function __construct(
         public string $name,
         public string $email,
         public ServiceTypeEnum $serviceType,
         public ServiceProviderStatusEnum $status,
+        public ?int $id,
         public ?string $url,
         public ?string $apiKey
     ){}
 
-    public static function fromArray(array $data): ServiceProviderDTO
+    public static function fromArray(array $data): ServiceProviderDto
     {
         return new self(
             name: $data['name'],
             email: $data['email'],
             serviceType: $data['service_type'],
             status: $data['status'],
+            id: $data['id'],
             url: $data['url'],
             apiKey: $data['api_key']
         );
@@ -35,6 +37,7 @@ class ServiceProviderDTO
             'email' => $this->email,
             'service_type' => $this->serviceType,
             'status' => $this->status,
+            'id' => $this->id,
             'url' => $this->url,
             'apiKey' => $this->apiKey,
         ];
